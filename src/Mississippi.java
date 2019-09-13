ffd26c86095a54c41292e5f769961151562be297
li_mic000 <li_mic000@CAM-LAB-28.bgsd.k12.wa.us>
2019-09-13 07:44:32 -0700

    MI

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -2,8 +2,29 @@
 public class Mississippi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		letter_M();
+		letter_I();
 	}
-
+	public static void letter_M() {
+		System.out.println("M       M");
+		System.out.println("MM     MM");
+		System.out.println("M M   M M");
+		System.out.println("M   M   M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+	}
+	public static void letter_I () {
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+	}
+	public static void letter_S () {
+		System.out.println("S");
+	}
+	public static void letter_P () {
+		System.out.println("P");
+	}
 }
